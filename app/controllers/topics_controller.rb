ec3005e2949974829e269984486c29b6660cb4a2
Alix Hamilton <alix.hamilton@gmail.com>
2016-06-18 19:22:04 -0700

    added formatting

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -65,7 +65,14 @@ class TopicsController < ApplicationController
     @topic.votes.create
     redirect_to(topics_path)
   end
+  def downvote
+    @topic = Topic.find(params[:id])
+    if @topic.votes.first
+      @topic.votes.first.delete
+    end
+    redirect_to(topics_path)
 
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
--- app/helpers/topics_helper.rb
+++ app/helpers/topics_helper.rb
@@ -1,2 +1,6 @@
 module TopicsHelper
+	def vote_bar_style(topic)
+		"width: #{topic.votes.count*4}px;" +
+		"background-color: ##{SecureRandom.hex(3)};"
+	end
 end
